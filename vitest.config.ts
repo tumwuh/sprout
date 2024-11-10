1568f91780ae21a3cb1491ed9a5075af72be283f
huntz <huntzrahmadi@gmail.com>
2024-11-10 15:04:59 +0700

    isolate both vitest and playwright test env

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
     test: {
         environment: 'nuxt',
+        exclude: ['e2e/**/*.spec.ts'],
         // you can optionally set Nuxt-specific environment options
         // environmentOptions: {
         //   nuxt: {
