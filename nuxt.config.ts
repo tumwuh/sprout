cde5a6bccebb2632c1a9da080722910b65ce26e9
huntz <huntzrahmadi@gmail.com>
2024-11-14 11:33:42 +0700

    issue when building using bun

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
     devtools: {enabled: true},
     nitro: {
         compressPublicAssets: true,
+        preset: 'bun',
         prerender: {
             routes: ['/'],
         },
