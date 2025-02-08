27fb287a0d2e63efe93e2d24b1d4d47fc91fcf6c
huntz <huntzrahmadi@gmail.com>
2025-02-08 22:16:13 +0700

    commit to check pipeline test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     devtools: {enabled: true},
     nitro: {
         compressPublicAssets: true,
-        preset: 'node',
+        preset: process.env.NODE_ENV === 'production' ? 'bun' : 'node',
         prerender: {
             routes: ['/'],
         },
